49d6a87c50d78d826ecbc12a84c56232d3962e70
Riwa Hammoud <riwahammoud1@gmail.com>
2024-04-14 14:47:12 +0300

    committt

--- routes/authHelper.js
+++ routes/authHelper.js
@@ -1,3 +1,6 @@
+const qs = require('querystring');
+const axios = require('axios');
+
 const Authorization=()=> {
     // client_id 
     // response_type
@@ -9,10 +12,29 @@ const Authorization=()=> {
     return encodeURI(`https://linkedin.com/oauth/v2/authorization?client_id=${process.env.CLIENT_ID}&response_type=code&scope=${process.env.SCOPE}&redirect_uri=${process.env.REDIRECT_URL}`);
 }
 
-const Redirect =(code)=> {
+const Redirect =async (code)=> {
     const payload = {
-        code
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        redirect_url: process.env.REDIRECT_URL,
+        grant_type: 'authorization code',
+        code: code
+
     }
+
+    const {data}= await axios({
+        url: `https://linkedin.com/oauth/v2/accessToken?${qs.stringify(payload)}`,
+        method: 'POST',
+        headers:{
+            'Content-Type': 'x-www-form-urlencoded'
+        }
+    }).then(response=>{
+        return response;
+    }).catch(error=>{
+        return error;
+    });
+
+    return data;
 }
 
 module.exports = {

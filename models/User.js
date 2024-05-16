8e3e27a17fdf979c30ba620dbc4b5b9b4fbda8a3
Riwa Hammoud <riwahammoud1@gmail.com>
2024-05-16 21:00:05 +0300

    user fixed

--- models/User.js
+++ models/User.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
     {
         username: {type: String, required: true, unique: true},
+        id: {type: String, unique: true},
         email: {type: String, required: true, unique: true},
         password: {type: String, required: true, unique: true},
         userType: { type: String, enum: ['company', 'normalUser'], default: 'normalUser' },
         location: {type: String, required: false},
         isAdmin: {type: Boolean, default: false},
         isAgent: {type: Boolean, default: false},
-        skills: {type: Array, default: []},
+        skills: {type: Array, default: [], required: false},
         profile: {
             type: String, 
             required: true, 
